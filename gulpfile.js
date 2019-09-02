3647dcf3c140d708efc707badda902ead1239c61
ajantabisht <ajanta.bisht@klarna.com>
2019-09-02 08:13:03 +0200

    travel site changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+ watch = require('gulp-watch');
+ postcss = require('gulp-postcss')
+ autoprefixer = require('autoprefixer')
+ cssvars = require('postcss-simple-vars')
+ nested =require('postcss-nested'),
+ cssImport = require('postcss-import');
 
 gulp.task('default',function(){
-    console.log('horray you created a gulp task.');
-});
+    console.log('hurray- you created the gulp task.')
+})
 
-gulp.task('html',function(){
-console.log('imagen someting usefull to be done to yout HTML');
-});
+gulp.task('html', function(){
+    console.log('Imagine someting useful deing done in your HTML')
+})
 
 gulp.task('styles',function(){
-    console.log('image post css task running ');
-    });
+   return gulp.src('./app/assets/styles/styles.css')
+   .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'))
+})
 
-gulp.task ('watch', function(){
-
-    watch('./app/index.html', function(){
-
-        gulp.start('html');
+gulp.task('watch', function(){
+    watch('./app/index.html',function(){
+        gulp.start('html')
     })
-
-    watch('./app/assets/styles/*.css', function(){
-
-        gulp.start('styles');
+    watch('./app/assets/styles/*.css',function(){
+        gulp.start('styles')
     })
-});
+})
